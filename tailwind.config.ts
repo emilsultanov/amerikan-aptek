c79b1862ccc3aa8fac517957f99f7f4088f165c4
emilsultanov <emilsultanov01@gmail.com>
2024-01-17 00:34:01 +0400

    Add basePath to start of image urls

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,8 +20,8 @@ const config: Config = {
         '3xl': '0 0 30px rgba(7,51,84,0.17)'
       },
       backgroundImage: {
-        'section-0': "radial-gradient(circle at top right,rgba(255,255,255,0) 20%,rgba(1,77,183,0.9) 100%),url('/pharmacy-05.jpg')",
-        'section-4': "radial-gradient(circle at top right,rgba(1,77,183,0.5) 0%,rgba(255,255,255,0) 100%),url('/pharmacy-06.jpg')"
+        'section-0': "radial-gradient(circle at top right,rgba(255,255,255,0) 20%,rgba(1,77,183,0.9) 100%),url('/amerikan-aptek/pharmacy-05.jpg')",
+        'section-4': "radial-gradient(circle at top right,rgba(1,77,183,0.5) 0%,rgba(255,255,255,0) 100%),url('/amerikan-aptek/pharmacy-06.jpg')"
       }
     },
   },
